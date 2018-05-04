0791cea77d2562c1d03ad9e899ee9d3542465269
romeric <roman_poya@yahoo.com>
2018-05-04 14:57:25 +0100

    Remove RunSession and mpi_runners as they are obsolete now

--- mpi_runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/Dropbox/florence
-
-# start=$SECONDS
-time mpirun -np 16 Florence/FiniteElements/DistributedAssembly.py "/home/roman/tmp/"
-# duration=$(( SECONDS - start ))
-# echo $duration
